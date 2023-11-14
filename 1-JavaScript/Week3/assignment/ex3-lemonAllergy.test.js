7dd3c3aab21fb2f5826e78c28d231e98bce9fbd4
tenzijn <tenzinkunchok2022@gmail.com>
2023-11-14 22:40:45 +0100

    ex3 completed and working

--- 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
@@ -26,26 +26,30 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasket, fruitToBeRemoved) {
+  const filteredBasket = fruitBasket.filter(
+    (fruit) => fruit !== fruitToBeRemoved
+  );
+  return filteredBasket;
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(fruitBasket).toEqual(originalFruitBasketContents);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    const filteredBasket = sanitizeFruitBasket(fruitBasket, 'lemon');
+    const expected = fruitBasket.filter((fruit) => fruit !== 'lemon');
+    expect(filteredBasket).toEqual(expected);
   });
 });
