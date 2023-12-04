0556969f1e487109e268e7fe6e2ffcd71507d7e8
tenzijn <tenzinkunchok2022@gmail.com>
2023-12-04 14:24:19 +0100

    minor changes

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -55,21 +55,18 @@ function main() {
       author: 'Don Norman',
       isbn: '978-0465050659',
       alreadyRead: false,
-      cover: './assets/the_design_of_everyday_things.jpg',
     },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       isbn: '978-1617933431',
       alreadyRead: true,
-      cover: './assets/the_most_human_human.jpg',
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
       alreadyRead: true,
-      cover: './assets/the_pragmatic_programmer.jpg',
     },
   ];
 
