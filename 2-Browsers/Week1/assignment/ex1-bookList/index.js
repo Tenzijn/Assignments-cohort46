a1737bbc0561e3761cf5b11e7c719df26c17cfd6
tenzijn <tenzinkunchok2022@gmail.com>
2023-12-04 14:22:52 +0100

    exercise 1 completed

--- 2-Browsers/Week1/assignment/ex1-bookList/index.js
+++ 2-Browsers/Week1/assignment/ex1-bookList/index.js
@@ -17,8 +17,35 @@ https://hackyourfuture.github.io/example-pages/Browsers/Week1/1-booklist/
 -----------------------------------------------------------------------------*/
 //cspell: enable
 
+// function to create the image link
+function bookCoverImg(title) {
+  const imgTitle = title.toLowerCase().replace(/ /g, '_') + '.jpg';
+  const imgLink = './assets/' + imgTitle;
+  return imgLink;
+}
+
 function createBookList(books) {
   // TODO your code goes in here, return the ul element
+  const bookList = document.createElement('ul');
+  books.forEach((book, index) => {
+    const eachBook = document.createElement('li');
+    const titleAndAuthor = document.createElement('p');
+    const bookCover = document.createElement('img');
+    titleAndAuthor.textContent = `Title : ${book.title} Author : ${book.author} `;
+    bookCover.setAttribute('src', bookCoverImg(books[index].title));
+    eachBook.append(titleAndAuthor, bookCover);
+
+    // to change the style of the book depending on whether you have read it(green) or not(red).
+    if (book.alreadyRead) {
+      eachBook.classList.add('green-background', 'book');
+    } else {
+      eachBook.classList.add('red-background', 'book');
+    }
+    // eachBook.appendChild(titleAndAuthor);
+    bookList.appendChild(eachBook);
+  });
+
+  return bookList;
 }
 
 function main() {
@@ -28,18 +55,21 @@ function main() {
       author: 'Don Norman',
       isbn: '978-0465050659',
       alreadyRead: false,
+      cover: './assets/the_design_of_everyday_things.jpg',
     },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       isbn: '978-1617933431',
       alreadyRead: true,
+      cover: './assets/the_most_human_human.jpg',
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
       alreadyRead: true,
+      cover: './assets/the_pragmatic_programmer.jpg',
     },
   ];
 
