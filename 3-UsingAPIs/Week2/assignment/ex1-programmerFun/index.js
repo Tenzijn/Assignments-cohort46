88113cb0f1306251ad2496e59382f698f608651e
tenzijn <tenzinkunchok2022@gmail.com>
2024-01-15 19:28:11 +0100

    ex1 complete with test

--- 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
@@ -17,29 +17,41 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
    url with `.shx`. There is no server at the modified url, therefore this 
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
+
+const dom = document.querySelector('body');
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  const res = fetch(url);
+  return res.then((response) => {
+    if (!response.ok) {
+      throw new Error('HTTP error! status: ${response.status}');
+    } else {
+      return response.json();
+    }
+  });
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
   console.log(data);
+  const img = document.createElement('img');
+  img.alt = data.title;
+  img.src = data.img;
+  dom.appendChild(img);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
   console.log(error);
+  const h1 = document.createElement('h1');
+  h1.textContent = error;
+  dom.appendChild(h1);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);
